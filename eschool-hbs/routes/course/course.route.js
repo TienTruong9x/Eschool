243ca934ba528cd70c0ae2d7d4df63d476a9f42d
TienTruong <nguyenthientruong1998@gmail.com>
2020-11-26 14:16:00 +0700

    Update Coure

--- eschool-hbs/routes/course/course.route.js
+++ eschool-hbs/routes/course/course.route.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 const courseController = require("../../app/Controllers/CourseController");
 
-router.get("/course", courseController.getCourse);
-router.get("/course/add", courseController.addCourse);
-router.post("/course/add", courseController.addCoursePost);
-router.get("/course/edit/:slug", courseController.editCourse);
-router.put("/course/edit/:slug", courseController.editCoursePut);
-router.delete("/course/delete/:id",courseController.deleteCourse);
+router.get("/", courseController.getCourse);
+router.get("/add", courseController.addCourse);
+router.post("/add", courseController.addCoursePost);
+router.get("/edit/:slug", courseController.editCourse);
+router.put("/edit/:slug", courseController.editCoursePut);
+router.delete("/delete/:id", courseController.deleteCourse);
 
 module.exports = router;
