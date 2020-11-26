db93adc6bb21e058a55ad165dd4852b9a4d4a9ac
TienTruong <nguyenthientruong1998@gmail.com>
2020-11-26 14:59:13 +0700

    Done Projects

--- eschool-hbs/routes/index.js
+++ eschool-hbs/routes/index.js
@@ -2,12 +2,14 @@ const authRoute = require("./auth/auth.route");
 const siteRoute = require("./site.route");
 const courseRoute = require("./course/course.route");
 const trainerRoute = require("./trainer/trainer.route");
+const projectsRoute = require("./projects/projects.route");
 
 function initRoutes(app) {
   app.use("/Auth", authRoute);
   app.use("/", siteRoute);
   app.use("/Me/course", courseRoute);
   app.use("/Admin/trainer", trainerRoute);
+  app.use("/Admin/projects", projectsRoute);
 }
 
 module.exports = initRoutes;
