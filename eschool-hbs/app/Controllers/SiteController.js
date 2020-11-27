8a3fbc025289914808305fe9217051a2064e9dde
TienTruong <nguyenthientruong1998@gmail.com>
2020-11-28 00:23:19 +0700

    Edi

--- eschool-hbs/app/Controllers/SiteController.js
+++ eschool-hbs/app/Controllers/SiteController.js
@@ -1,7 +1,27 @@
+const Course = require("../Models/Coure.model");
+const Project = require("../Models/Project.model");
+const Trainer = require("../Models/Trainer.model");
+const Post = require("../Models/Post.model");
+
 class SiteController {
   //[GET] /
   index(req, res, next) {
-    res.render("index", { title: "Trang Chủ | Nguyễn Tiến Trường" });
+    var course = Course.find({});
+    var trainer = Trainer.find({}).limit(4);
+    var post = Post.find({}).limit(3);
+    var project = Project.find({}).limit(6);
+    Promise.all([course, trainer, post, project]).then(
+      ([courses, trainers, posts, projects]) => {
+        console.log(courses);
+        res.render("index", {
+          title: "Trang Chủ | Nguyễn Tiến Trường",
+          courses,
+          trainers,
+          posts,
+          projects,
+        });
+      }
+    );
   }
 }
 
